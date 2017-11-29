a120107e278fc467ab47df19a44921a838aeda82
Kleyson Morais <kleysonb@hotmail.com>
2017-11-29 15:42:28 -0300

    implementado para a base de dados referida

--- k-means.py
+++ k-means.py
@@ -40,7 +40,7 @@ def gerarMatriz(centroide, list_coordenadas):
     refinarCentroide(centroide, list_coordenadas)
     return alteracoes
 
-def plota_dispersao(list_coordenadas):
+def plota_dispersao(list_coordenadas, centroide):
     fig = plt.figure()
 
     ax1 = fig.add_subplot(121)
@@ -50,8 +50,16 @@ def plota_dispersao(list_coordenadas):
         if(list_coordenadas[index][9] == 1):
             x.append(list_coordenadas[index][2])
             y.append(list_coordenadas[index][5])
-    ax1.scatter(x, y, color = "blue", s = 3, edgecolor='none')
+    ax1.scatter(x, y, color = "blue", s = 6, edgecolor='none')
     ax1.set_aspect(1. / ax1.get_data_ratio())
+
+    ax3 = fig.add_subplot(121)
+    x3 = []
+    y3 = []
+    x3.append(centroide[0][2])
+    y3.append(centroide[0][5])
+    ax3.scatter(x3, y3, color="blue", s=30, edgecolor='none')
+    ax3.set_aspect(1. / ax1.get_data_ratio())
 
     ax2 = fig.add_subplot(121)
     x1 = []
@@ -60,33 +68,112 @@ def plota_dispersao(list_coordenadas):
         if (list_coordenadas[index][9] == 2):
             x1.append(list_coordenadas[index][2])
             y1.append(list_coordenadas[index][5])
-    ax2.scatter(x1, y1, color="red", s=3, edgecolor='none')
+    ax2.scatter(x1, y1, color="red", s=6, edgecolor='none')
     ax2.set_aspect(1. / ax2.get_data_ratio())
+
+    ax4 = fig.add_subplot(121)
+    x4 = []
+    y4 = []
+    x4.append(centroide[1][2])
+    y4.append(centroide[1][5])
+    ax4.scatter(x4, y4, color="red", s=30, edgecolor='none')
+    ax4.set_aspect(1. / ax1.get_data_ratio())
 
     plt.show()
 
 def refinarCentroide(centroide, list_coordenadas):
+    print "Refinando Centroides"
     for i in range(len(centroide)):
         cont = 0
+        atributo1 = 0
+        atributo2 = 0
+        atributo3 = 0
+        atributo4 = 0
+        atributo5 = 0
+        atributo6 = 0
+        atributo7 = 0
+        atributo8 = 0
+        atributo9 = 0
 
-
-
-
-
-        
-        somaX = 0
-        somaY = 0
         for j in range(len(list_coordenadas)):
-            if (list_coordenadas[j][2] == i+1):
-                somaX += list_coordenadas[j][0]
-                somaY += list_coordenadas[j][1]
+            if (list_coordenadas[j][9] == i+1):
+                atributo1 += list_coordenadas[j][0]
+                atributo2 += list_coordenadas[j][1]
+                atributo3 += list_coordenadas[j][2]
+                atributo4 += list_coordenadas[j][3]
+                atributo5 += list_coordenadas[j][4]
+                atributo6 += list_coordenadas[j][5]
+                atributo7 += list_coordenadas[j][6]
+                atributo8 += list_coordenadas[j][7]
+                atributo9 += list_coordenadas[j][8]
                 cont += 1
-        centroide[i][0] = somaX/cont
-        centroide[i][1] = somaY/cont
+        centroide[i][0] = atributo1/cont
+        centroide[i][1] = atributo2/cont
+        centroide[i][2] = atributo3/cont
+        centroide[i][3] = atributo4/cont
+        centroide[i][4] = atributo5/cont
+        centroide[i][5] = atributo6/cont
+        centroide[i][6] = atributo7/cont
+        centroide[i][7] = atributo8/cont
+        centroide[i][8] = atributo9/cont
+
 
     print "Novos Centroides:"
     for index in range(len(centroide)):
         print centroide[index]
+
+def acerto(resultado, list_coordenadas):
+
+    classe1 = 0
+    classe2 = 0
+    classe3 = 0
+    classe4 = 0
+
+    benigno = 444
+    maligno = 239
+
+    for index in range(len(list_coordenadas)):
+        if (resultado[index] == 1 and list_coordenadas[index][9] == 1):
+            classe1+=1
+        elif (resultado[index] == 2 and list_coordenadas[index][9] == 2):
+            classe2+=1
+
+    for index in range(len(list_coordenadas)):
+        if (resultado[index] == 1 and list_coordenadas[index][9] == 2):
+            classe3+=1
+        elif (resultado[index] == 2 and list_coordenadas[index][9] == 1):
+            classe4+=1
+
+    b = 0
+    m = 0
+    if(classe1 > classe3):
+        if (classe1 > classe2):
+            print "Benigno: " + str(classe1)
+            print "Maligno: " + str(classe2)
+            b = abs(classe1 - benigno)
+            m = abs(classe2 - maligno)
+        else:
+            print "Benigno: " + str(classe2)
+            print "Maligno: " + str(classe1)
+            b = abs(classe2 - benigno)
+            m = abs(classe1 - maligno)
+    else:
+        if(classe3 > classe4):
+            print "Benigno: " + str(classe3)
+            print "Maligno: " + str(classe4)
+            b = abs(classe3 - benigno)
+            m = abs(classe4 - maligno)
+        else:
+            print "Benigno: " + str(classe3)
+            print "Maligno: " + str(classe4)
+            b = abs(classe4 - benigno)
+            m = abs(classe3 - maligno)
+
+    b = (b*100)/benigno
+    m = (m*100)/maligno
+
+    print "Taxa de Erro Benigno: " + str(b) + "%"
+    print "Taxa de Erro Maligno: " + str(m) + "%"
 
 # MAIN passando os parametros para o algoritmo
 if __name__ == "__main__":
@@ -96,6 +183,7 @@ if __name__ == "__main__":
     nome_tabela = "Classificacao"
 
     list_coordenadas = []
+    resultado = []
 
     #Lendo o arquivo
     with open(arquivo_csv, 'rb') as ficheiro:
@@ -106,11 +194,16 @@ if __name__ == "__main__":
                 ponto = [float(linha[1]), float(linha[2]), float(linha[3]), float(linha[4]), float(linha[5]), float(linha[6]), 
                 float(linha[7]), float(linha[8]), float(linha[9]), random.randint(1, 2)]
                 #print (ponto)
+                if(linha[10] == "2"):
+                    #Benigno
+                    resultado.append(1)
+                else:
+                    #Maligno
+                    resultado.append(2)
                 list_coordenadas.append(ponto)
         except csv.Error as e:
             sys.exit('ficheiro %s, linha %d: %s' % (arquivo_csv, reader.line_num, e))
 
-    
     centroide = []
     aux = len(list_coordenadas) - 1
     for index in range(k):
@@ -123,15 +216,18 @@ if __name__ == "__main__":
         print centroide[index]
 
     
-    plota_dispersao(list_coordenadas)
+    #plota_dispersao(list_coordenadas, centroide)
     
     
     alteracoes = gerarMatriz(centroide, list_coordenadas)
     
-    '''
+
     while(alteracoes):
         alteracoes = gerarMatriz(centroide, list_coordenadas)
+        #plota_dispersao(list_coordenadas, centroide)
+    plota_dispersao(list_coordenadas, centroide)
 
-        plota_dispersao(list_coordenadas)
+    acerto(resultado, list_coordenadas)
+
     print "Finalizado"
-    '''
+
